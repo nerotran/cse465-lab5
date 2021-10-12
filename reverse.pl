d415cc380a00ae09e776b5e77313afc214f89cb2
Nero Tran <tranhuq@miamioh.edu>
2021-10-11 21:52:52 -0400

    Update

--- reverse.pl
+++ reverse.pl
@@ -9,7 +9,7 @@
 %            if Result is [c, b, a] then helper is True
 
 % TODO: Implement my_reverse  (you need to implement helper)
-% my_reverse(L1, L2) :- helper(L1, L2, []).
+my_reverse(L1, L2) :- helper(L1, L2, []).
 helper([], L2, L3) :- L2 = L3.
 helper([H|T], L2, L3) :- append(H, L3, Temp), helper(T, L2, Temp).
 
